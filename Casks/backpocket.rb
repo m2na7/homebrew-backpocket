136143cc31d8e43a2ba88cdfbab80996bfa52891
m2na7 <118591632+m2na7@users.noreply.github.com>
2026-09-02 00:31:57 +0900

    chore: bump the cask to 0.1.4

--- Casks/backpocket.rb
+++ Casks/backpocket.rb
@@ -1,6 +1,6 @@
 cask "backpocket" do
-  version "0.1.3"
-  sha256 "4acb631760bdcdfc95ca1461ff8d596d52a3417d8a2944d4390aee3de1df65ce"
+  version "0.1.4"
+  sha256 "98cbb6b48d770fe4f38150aa914edbbedd60af1b4f1156b3815c166ac27cdd08"
 
   url "https://github.com/m2na7/Backpocket/releases/download/v#{version}/Backpocket.zip",
       verified: "github.com/m2na7/Backpocket/"
